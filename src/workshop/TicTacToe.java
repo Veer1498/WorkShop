bfc086a802d662d066cb7d2760a3481e59936eca
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-27 08:48:08 +0530

    [Veer] Added : Created a TicTacToe Board with empty Spaces

--- src/workshop/TicTacToe.java
+++ src/workshop/TicTacToe.java
@@ -1,10 +1,37 @@
 package workshop;
+import java.util.Scanner;
+/**
+ * Tic-Tac-Toe Game Simulation
+ * @author Veer.Singa
+ *
+ */
 
+	//TicTacToe Class
 public class TicTacToe {
-
+	/**
+	 * 1.Created an Array Board with 10 characters.
+	 * 2.Imported a scanner 
+	 * 3.Created a method createBoard, added a For Loop where i up to array length.
+	 * 4.board[i] will be stored empty.
+	 * 5.Main function with welcome message is created and createBoard function is called.
+	 * 
+	 */
+	static char[] board = new char[10];
+	static Scanner scanner = new Scanner(System.in);
+	
+	//UC1- Creating an Empty Board
+	public static void createBoard() {
+		for (int i=1;i<board.length;i++) {
+			board[i] = ' ';
+		}
+}
+	
+	//Main Function
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Tic Tac Toe Game");
+		createBoard();
+		
 	}
 
 }
