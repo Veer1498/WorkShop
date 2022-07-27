f16661a81b6effe1bc7f4f5dc5d822ff6bd835d6
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-27 13:32:22 +0530

    [Veer] Added : Created a TicTacToe Board with empty Spaces

--- src/workshop/TicTacToe.java
+++ src/workshop/TicTacToe.java
@@ -1,45 +1,55 @@
 package workshop;
 /**
- * Tic-Tac-Toe Game Simulation.
+ * Create a TicTacToe Game Simulation.
  * @author Veer.Singa
  *
  */
-
 	//TicTacToe Class
 public class TicTacToe {
 	/**
-	 * 1.Created an Array Board with 10 characters.
+	 * 1.Created an Array Board which takes 10 characters.
 	 * 2.Imported a scanner 
 	 * 3.Created a method createBoard, added a For Loop where i up to array length.
 	 * 4.board[i] will be stored empty.
-	 * 5.Main function with welcome message is created and createBoard function is called.
+	 * 5.Created a showBoard Function and arranged the method output as required.
+	 * 6.Main function with welcome message is created and createBoard function is called.
 	 * 
 	 */
 	
 	//UC1- Creating an Empty Board
 	
 	static char[] gameBoard = new char[10];
+	
+	//Welcome Function
+	public static void welcome() {
+		System.out.println("Welcome to Tic Tac Toe Game");
+		System.out.println("***************************");
+		System.out.println("");
+	}
 		
 	//createBoard Function
-	public static void createBoard() {
+	public static char[] createBoard() {
 		for (int i=1;i<gameBoard.length;i++) {
 			gameBoard[i] = ' ';
 		}
+		return gameBoard;
 	}
-	public static void showBoard() {
-		System.out.println(gameBoard[1]+"|"+gameBoard[2]+"|"+gameBoard[3]+"|");
-		System.out.println("-------");
-		System.out.println(gameBoard[4]+"|"+gameBoard[5]+"|"+gameBoard[6]+"|");
-		System.out.println("-------");
-		System.out.println(gameBoard[7]+"|"+gameBoard[8]+"|"+gameBoard[9]+"|");
-	}
+	//showBoard Function
+//	public static void showBoard() {
+//		System.out.println(gameBoard[1]+"|"+gameBoard[2]+"|"+gameBoard[3]+"|");
+//		System.out.println("-------");
+//		System.out.println(gameBoard[4]+"|"+gameBoard[5]+"|"+gameBoard[6]+"|");
+//		System.out.println("-------");
+//		System.out.println(gameBoard[7]+"|"+gameBoard[8]+"|"+gameBoard[9]+"|");
+//	}
 	
 	//Main Function
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Tic Tac Toe Game");
-		createBoard();
-		showBoard();
+		//Calling Required Methods
+		welcome();
+		char [] game =createBoard();
+	//	showBoard();
 		
 	}
 
